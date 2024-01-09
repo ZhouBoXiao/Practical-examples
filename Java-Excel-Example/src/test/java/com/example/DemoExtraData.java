8863aa15dc3c161c166bd688e00db5b66901495e
ZhouBoXiao <1533880208@qq.com>
2024-01-10 00:25:20 +0800

    add java-excel-example

---
+++ Java-Excel-Example/src/test/java/com/example/DemoExtraData.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class DemoExtraData {
+
+    private String row1;
+
+    private String row2;
+}
