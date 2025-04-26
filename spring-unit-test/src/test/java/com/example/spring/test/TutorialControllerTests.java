cd67c86faa3ec817bdafedeb279c2f4d9679afa0
ZhouBoXiao <1533880208@qq.com>
2025-04-26 23:36:30 +0800

    update TutorialControllerTests

--- spring-unit-test/src/test/java/com/example/spring/test/TutorialControllerTests.java
+++ spring-unit-test/src/test/java/com/example/spring/test/TutorialControllerTests.java
@@ -29,143 +29,168 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(TutorialController.class)
 public class TutorialControllerTests {
-  @MockBean
-  private TutorialRepository tutorialRepository;
+    @MockBean
+    private TutorialRepository tutorialRepository;
 
-  @Autowired
-  private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @Autowired
-  private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-  @Test
-  void shouldCreateTutorial() throws Exception {
-    Tutorial tutorial = new Tutorial(1, "Spring Boot @WebMvcTest", "Description", true);
+    @Test
+    void shouldCreateTutorial() throws Exception {
+        Tutorial tutorial = new Tutorial(1, "Spring Boot @WebMvcTest", "Description", true);
 
-    mockMvc.perform(post("/api/tutorials").contentType(MediaType.APPLICATION_JSON)
-        .content(objectMapper.writeValueAsString(tutorial)))
-        .andExpect(status().isCreated())
-        .andDo(print());
-  }
+        mockMvc.perform(post("/api/tutorials").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(tutorial)))
+                .andExpect(status().isCreated())
+                .andDo(print());
+    }
 
-  @Test
-  void shouldReturnTutorial() throws Exception {
-    long id = 1L;
-    Tutorial tutorial = new Tutorial(id, "Spring Boot @WebMvcTest", "Description", true);
+    @Test
+    void shouldReturnTutorial() throws Exception {
+        long id = 1L;
+        Tutorial tutorial = new Tutorial(id, "Spring Boot @WebMvcTest", "Description", true);
 
-    when(tutorialRepository.findById(id)).thenReturn(Optional.of(tutorial));
-    mockMvc.perform(get("/api/tutorials/{id}", id)).andExpect(status().isOk())
-        .andExpect(jsonPath("$.id").value(id))
-        .andExpect(jsonPath("$.title").value(tutorial.getTitle()))
-        .andExpect(jsonPath("$.description").value(tutorial.getDescription()))
-        .andExpect(jsonPath("$.published").value(tutorial.isPublished()))
-        .andDo(print());
-  }
+        when(tutorialRepository.findById(id)).thenReturn(Optional.of(tutorial));
+        mockMvc.perform(get("/api/tutorials/{id}", id)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.title").value(tutorial.getTitle()))
+                .andExpect(jsonPath("$.description").value(tutorial.getDescription()))
+                .andExpect(jsonPath("$.published").value(tutorial.isPublished()))
+                .andDo(print());
+    }
 
-  @Test
-  void shouldReturnNotFoundTutorial() throws Exception {
-    long id = 1L;
+    @Test
+    void shouldReturnNotFoundTutorial() throws Exception {
+        long id = 1L;
 
-    when(tutorialRepository.findById(id)).thenReturn(Optional.empty());
-    mockMvc.perform(get("/api/tutorials/{id}", id))
-         .andExpect(status().isNotFound())
-         .andDo(print());
-  }
+        when(tutorialRepository.findById(id)).thenReturn(Optional.empty());
+        mockMvc.perform(get("/api/tutorials/{id}", id))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
 
-  @Test
-  void shouldReturnListOfTutorials() throws Exception {
-    List<Tutorial> tutorials = new ArrayList<>(
-        Arrays.asList(new Tutorial(1, "Spring Boot @WebMvcTest 1", "Description 1", true),
-            new Tutorial(2, "Spring Boot @WebMvcTest 2", "Description 2", true),
-            new Tutorial(3, "Spring Boot @WebMvcTest 3", "Description 3", true)));
+    @Test
+    void shouldReturnListOfTutorials() throws Exception {
+        List<Tutorial> tutorials = new ArrayList<>(
+                Arrays.asList(new Tutorial(1, "Spring Boot @WebMvcTest 1", "Description 1", true),
+                        new Tutorial(2, "Spring Boot @WebMvcTest 2", "Description 2", true),
+                        new Tutorial(3, "Spring Boot @WebMvcTest 3", "Description 3", true)));
 
-    when(tutorialRepository.findAll()).thenReturn(tutorials);
-    mockMvc.perform(get("/api/tutorials"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.size()").value(tutorials.size()))
-        .andDo(print());
-  }
+        when(tutorialRepository.findAll()).thenReturn(tutorials);
+        mockMvc.perform(get("/api/tutorials"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(tutorials.size()))
+                .andDo(print());
+    }
 
-  @Test
-  void shouldReturnListOfTutorialsWithFilter() throws Exception {
-    List<Tutorial> tutorials = new ArrayList<>(
-        Arrays.asList(new Tutorial(1, "Spring Boot @WebMvcTest", "Description 1", true),
-            new Tutorial(3, "Spring Boot Web MVC", "Description 3", true)));
+    @Test
+    void shouldReturnListOfTutorialsWithFilter() throws Exception {
+        List<Tutorial> tutorials = new ArrayList<>(
+                Arrays.asList(new Tutorial(1, "Spring Boot @WebMvcTest", "Description 1", true),
+                        new Tutorial(3, "Spring Boot Web MVC", "Description 3", true)));
 
-    String title = "Boot";
-    MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
-    paramsMap.add("title", title);
+        String title = "Boot";
+        MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
+        paramsMap.add("title", title);
 
-    when(tutorialRepository.findByTitleContaining(title)).thenReturn(tutorials);
-    mockMvc.perform(get("/api/tutorials").params(paramsMap))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.size()").value(tutorials.size()))
-        .andDo(print());
-  }
-  
-  @Test
-  void shouldReturnNoContentWhenFilter() throws Exception {
-    String title = "BezKoder";
-    MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
-    paramsMap.add("title", title);
-    
-    List<Tutorial> tutorials = Collections.emptyList();
+        when(tutorialRepository.findByTitleContaining(title)).thenReturn(tutorials);
+        mockMvc.perform(get("/api/tutorials").params(paramsMap))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(tutorials.size()))
+                .andDo(print());
+    }
 
-    when(tutorialRepository.findByTitleContaining(title)).thenReturn(tutorials);
-    mockMvc.perform(get("/api/tutorials").params(paramsMap))
-        .andExpect(status().isNoContent())
-        .andDo(print());
-  }
+    @Test
+    void shouldReturnNoContentWhenFilter() throws Exception {
+        String title = "BezKoder";
+        MultiValueMap<String, String> paramsMap = new LinkedMultiValueMap<>();
+        paramsMap.add("title", title);
 
-  @Test
-  void shouldUpdateTutorial() throws Exception {
-    long id = 1L;
+        List<Tutorial> tutorials = Collections.emptyList();
 
-    Tutorial tutorial = new Tutorial(id, "Spring Boot @WebMvcTest", "Description", false);
-    Tutorial updatedtutorial = new Tutorial(id, "Updated", "Updated", true);
+        when(tutorialRepository.findByTitleContaining(title)).thenReturn(tutorials);
+        mockMvc.perform(get("/api/tutorials").params(paramsMap))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
 
-    when(tutorialRepository.findById(id)).thenReturn(Optional.of(tutorial));
-    when(tutorialRepository.save(any(Tutorial.class))).thenReturn(updatedtutorial);
+    @Test
+    void shouldUpdateTutorial() throws Exception {
+        long id = 1L;
 
-    mockMvc.perform(put("/api/tutorials/{id}", id).contentType(MediaType.APPLICATION_JSON)
-        .content(objectMapper.writeValueAsString(updatedtutorial)))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.title").value(updatedtutorial.getTitle()))
-        .andExpect(jsonPath("$.description").value(updatedtutorial.getDescription()))
-        .andExpect(jsonPath("$.published").value(updatedtutorial.isPublished()))
-        .andDo(print());
-  }
-  
-  @Test
-  void shouldReturnNotFoundUpdateTutorial() throws Exception {
-    long id = 1L;
+        Tutorial tutorial = new Tutorial(id, "Spring Boot @WebMvcTest", "Description", false);
+        Tutorial updatedtutorial = new Tutorial(id, "Updated", "Updated", true);
 
-    Tutorial updatedtutorial = new Tutorial(id, "Updated", "Updated", true);
+        when(tutorialRepository.findById(id)).thenReturn(Optional.of(tutorial));
+        when(tutorialRepository.save(any(Tutorial.class))).thenReturn(updatedtutorial);
 
-    when(tutorialRepository.findById(id)).thenReturn(Optional.empty());
-    when(tutorialRepository.save(any(Tutorial.class))).thenReturn(updatedtutorial);
+        mockMvc.perform(put("/api/tutorials/{id}", id).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedtutorial)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value(updatedtutorial.getTitle()))
+                .andExpect(jsonPath("$.description").value(updatedtutorial.getDescription()))
+                .andExpect(jsonPath("$.published").value(updatedtutorial.isPublished()))
+                .andDo(print());
+    }
 
-    mockMvc.perform(put("/api/tutorials/{id}", id).contentType(MediaType.APPLICATION_JSON)
-        .content(objectMapper.writeValueAsString(updatedtutorial)))
-        .andExpect(status().isNotFound())
-        .andDo(print());
-  }
-  
-  @Test
-  void shouldDeleteTutorial() throws Exception {
-    long id = 1L;
+    @Test
+    void shouldReturnNotFoundUpdateTutorial() throws Exception {
+        long id = 1L;
 
-    doNothing().when(tutorialRepository).deleteById(id);
-    mockMvc.perform(delete("/api/tutorials/{id}", id))
-         .andExpect(status().isNoContent())
-         .andDo(print());
-  }
-  
-  @Test
-  void shouldDeleteAllTutorials() throws Exception {
-    doNothing().when(tutorialRepository).deleteAll();
-    mockMvc.perform(delete("/api/tutorials"))
-         .andExpect(status().isNoContent())
-         .andDo(print());
-  }
+        Tutorial updatedtutorial = new Tutorial(id, "Updated", "Updated", true);
+
+        when(tutorialRepository.findById(id)).thenReturn(Optional.empty());
+        when(tutorialRepository.save(any(Tutorial.class))).thenReturn(updatedtutorial);
+
+        mockMvc.perform(put("/api/tutorials/{id}", id).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedtutorial)))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Test
+    void shouldDeleteTutorial() throws Exception {
+        long id = 1L;
+
+        doNothing().when(tutorialRepository).deleteById(id);
+        mockMvc.perform(delete("/api/tutorials/{id}", id))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
+
+    @Test
+    void shouldDeleteAllTutorials() throws Exception {
+        doNothing().when(tutorialRepository).deleteAll();
+        mockMvc.perform(delete("/api/tutorials"))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
+
+    @Test
+    void shouldReturnPublishedTutorials() throws Exception {
+        List<Tutorial> tutorials = new ArrayList<>(
+                Arrays.asList(new Tutorial(1, "Spring Boot @WebMvcTest 1", "Description 1", true),
+                        new Tutorial(2, "Spring Boot @WebMvcTest 2", "Description 2", true)));
+
+        when(tutorialRepository.findByPublished(true)).thenReturn(tutorials);
+        mockMvc.perform(get("/api/tutorials/published"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(tutorials.size()))
+                .andExpect(jsonPath("$[0].published").value(true))
+                .andExpect(jsonPath("$[1].published").value(true))
+                .andDo(print());
+    }
+
+    @Test
+    void shouldReturnNoContentWhenNoPublishedTutorials() throws Exception {
+        List<Tutorial> tutorials = Collections.emptyList();
+
+        when(tutorialRepository.findByPublished(true)).thenReturn(tutorials);
+        mockMvc.perform(get("/api/tutorials/published"))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
 }
